6dd3f3a6362c88f2d3a1b43a62ad629ed3e860cd
Satenderrathee <satender.rathee07@gmail.com>
2024-06-25 22:29:43 +0100

    updated as per review comments

--- example/provider.tf
+++ example/provider.tf
@@ -1,4 +1,13 @@
 terraform {
+
+  required_version = ">= 0.13"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.108.0"
+    }
+  }
+
   backend "azurerm" {
     resource_group_name  = "ResourceGroupName"  # Name of the resource group that your storage account resides in.
     storage_account_name = "StorageAccountName" # Name of the storage account for your terraform state file.
